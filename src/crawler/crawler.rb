c112d16efa2265a08f92978f0e021a6a49875fad
angelinn <angelin.nedelchev@outlook.com>
2017-12-28 20:00:18 +0200

    Fetch categories.

--- src/crawler/crawler.rb
+++ src/crawler/crawler.rb
@@ -1,7 +1,19 @@
 require 'nokogiri'
+require 'open-uri'
 
 class BgMammaCrawler
+    BASE_URL = 'http://www.bg-mamma.com'
+
     def initialize
-        puts "hi, I'm created"
+        puts "Crawler initialized"
+    end
+
+    def crawl
+        get_categories.each { |n| puts n['href'] }
+    end
+
+    def get_categories
+        page = Nokogiri::HTML(open(BASE_URL))
+        page.css('li.uk-parent a').select { |n| n['href'].include?('board') }
     end
 end
--- src/crawler/main.rb
+++ src/crawler/main.rb
@@ -1,3 +1,4 @@
 require_relative 'crawler'
 
 crawler = BgMammaCrawler.new
+crawler.crawl
