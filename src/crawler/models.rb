93d46d5e84c1f514298ee35fff3bfd6e01ba8cb7
angelinn <angelin.nedelchev@outlook.com>
2017-12-28 23:42:44 +0200

    Add Comment model.

--- src/crawler/models.rb
+++ src/crawler/models.rb
@@ -11,3 +11,15 @@ class Topic
         "#{self.parent} - #{self.url}\n"
     end
 end
+
+class Comment
+    attr_accessor :user
+    attr_accessor :content
+    attr_accessor :topic
+
+    def initialize(user, content, topic)
+        self.user = user
+        self.content = content
+        self.topic = topic
+    end
+end
