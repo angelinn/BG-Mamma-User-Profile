4a19d3a0c5544652ca21807ef4ec33024532596b
angelinn <angelin.nedelchev@outlook.com>
2017-12-28 23:33:39 +0200

    Crawl all the pages of a topic.

--- src/crawler/crawler.rb
+++ src/crawler/crawler.rb
@@ -16,14 +16,29 @@ class BgMammaCrawler
     end
 
     def get_categories
+        puts "Getting categories for #{BASE_URL}"
+
         page = Nokogiri::HTML(open(BASE_URL))
         page.css('li.uk-parent a').select { |n| n['href'].include?('board') }
     end
 
     def get_topics(categories)
         categories.map do |c|
+            puts "Getting topics for #{c['href']}"
+
             category = Nokogiri::HTML(open("#{BASE_URL}#{c['href']}"))
-            category.css('p.topic-title a').map { |t| Topic.new(c['href'], t['href']) } 
+
+            topics = []
+
+            loop do
+                page = category.css('li.uk-active').first            
+                topics << category.css('p.topic-title a').map { |t| Topic.new(c['href'], t['href']) } 
+                break unless page.next['uk-disabled'].nil?
+
+                puts "Opening #{BASE_URL}#{page.next.css('a').first['href']}"
+                category = Nokogiri::HTML(open("#{BASE_URL}#{page.next.css('a').first['href']}"))
+            end
+            topics
         end
     end
 end
