f658599d61cda04e49ccdd496aca4d4cc4d3e255
angelinn <angelin.nedelchev@outlook.com>
2017-12-29 16:49:16 +0200

    Add page crawling for comments.

--- src/crawler/crawler.rb
+++ src/crawler/crawler.rb
@@ -14,10 +14,9 @@ class BgMammaCrawler
     def crawl
         categories = get_categories
         puts "Got #{categories.count} categories."
-        categories.each do |c|
+        categories[0..2].each do |c|
             c.topics = get_topics(c)
-            c.topics.each { |t| t.comments = get_comments(t); break }
-            break
+            c.topics[0..2].each { |t| t.comments = get_comments(t); }
         end
 
         JsonSerializer.serialize categories
@@ -39,7 +38,7 @@ class BgMammaCrawler
         loop do
             page = category.css('li.uk-active').first            
             topics << category.css('p.topic-title a').map { |t| Topic.new(t['href']) } 
-            break unless page.next['uk-disabled'].nil?
+            break if page.nil? || (not page.next['uk-disabled'].nil?)
 
             puts "Opening #{BASE_URL}#{page.next.css('a').first['href']}"
             category = Nokogiri::HTML(open("#{BASE_URL}#{page.next.css('a').first['href']}"))
@@ -51,14 +50,26 @@ class BgMammaCrawler
     def get_comments(topic)
         topic_url = "#{BASE_URL}#{topic.url}"
         puts "Getting comments for #{topic_url}"
+        html = Nokogiri::HTML(open(topic_url))
+        comments = []
+        c = 0
+        loop do
+            break if c == 3
+            page = html.css('li.uk-active').first  
+            c += 1          
+            puts "Page #{page.text rescue 'last'}..."
+            html.css('div.topic-post.tpl3').map do |post|
+                user_name = post.css('p.user-name a').first
+                user = User.new(user_name['href'], user_name.text.strip)
+                content = post.css('div.post-content-inner').first.text.strip
+                date = post.css('span.post-date').text
+    
+                comments << Comment.new(user, content, date)
+            end
 
-        Nokogiri::HTML(open(topic_url)).css('div.topic-post.tpl3').map do |post|
-            user_name = post.css('p.user-name a').first
-            user = User.new(user_name['href'], user_name.text.strip)
-            content = post.css('div.post-content-inner').first.text.strip
-            date = post.css('span.post-date').text
-
-            Comment.new(user, content, date)
+            break if page.nil? || (not page.next['uk-disabled'].nil?)
+            html = Nokogiri::HTML(open("#{BASE_URL}#{page.next.css('a').first['href']}"))            
         end
+        comments
     end
 end
