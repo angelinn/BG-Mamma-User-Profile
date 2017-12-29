e9769a508d0587d22a4ce346935bb07b9e4554bd
angelinn <angelin.nedelchev@outlook.com>
2017-12-29 13:34:15 +0200

    Add date to comment.

--- src/crawler/models.rb
+++ src/crawler/models.rb
@@ -16,14 +16,16 @@ class Comment
     attr_accessor :user
     attr_accessor :content
     attr_accessor :topic
+    attr_accessor :date
 
-    def initialize(user, content, topic)
+    def initialize(user, content, topic, date)
         self.user = user
         self.content = content
         self.topic = topic
+        self.date = date
     end
 end
-
+    
 class User
     attr_accessor :profile_url
     attr_accessor :user_name
