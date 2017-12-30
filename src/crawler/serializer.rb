48990515e501dd881e71b20b87a3b2242593aa8b
angelinn <angelin.nedelchev@outlook.com>
2017-12-30 12:45:21 +0200

    Fix file name forbidden characters.

--- src/crawler/serializer.rb
+++ src/crawler/serializer.rb
@@ -3,6 +3,7 @@ require 'oj'
 class JsonSerializer
     class << self
     def serialize(object, file_name)
+        file_name.gsub!(/[\x00\\:\*\?\"<>\|]/, '_')       
         File.write(file_name, (Oj::dump object, :indent => 2, :use_as_json => true))
     end
     end
