3340833e188996f58acbf101ec7892ec9b27f0ea
angelinn <angelin.nedelchev@outlook.com>
2018-01-25 19:38:35 +0200

    Add escape method.

--- src/crawler/serializer.rb
+++ src/crawler/serializer.rb
@@ -3,8 +3,13 @@ require 'oj'
 class JsonSerializer
     class << self
     def serialize(object, file_name)
-        file_name.gsub!(/[\x00\\:\*\?\"<>\|]/, '_')       
+        file_name = escape(file_name)
+        puts file_name
         File.write(file_name, (Oj::dump object, :indent => 2, :use_as_json => true))
+    end
+
+    def escape(file_name)
+        file_name.gsub(/[\x00\\:\*\?\"<>\|]/, '_')       
     end
     end
 end
