f794e3ddec8267d131ca1665122aa3eabd7644ee
angelinn <angelin.nedelchev@outlook.com>
2018-01-27 03:15:56 +0200

    Add datalayer library.

---
+++ src/BGMammaProfileCreator/DataLayer/UserDomain.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer
+{
+    public class UserDomain
+    {
+        public static void CreateTables()
+        {
+            // SQLiteConnection connection = new SQLiteConnection("users.db");
+        }
+
+        public static void AddUser()
+        {
+
+        }
+    }
+}
