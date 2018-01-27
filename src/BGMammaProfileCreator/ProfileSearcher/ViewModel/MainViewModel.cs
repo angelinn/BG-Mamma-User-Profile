16f6a6df23ff4429c06ccbe1157eeb33ba7aa616
angelinn <angelin.nedelchev@outlook.com>
2018-01-27 14:04:11 +0200

    Implement VM logic.

--- src/BGMammaProfileCreator/ProfileSearcher/ViewModel/MainViewModel.cs
+++ src/BGMammaProfileCreator/ProfileSearcher/ViewModel/MainViewModel.cs
@@ -1,21 +1,51 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using ProfileCreator;
 using ProfileCreator.Models.Analyzed;
+using ProfileSearcher.ProfileSearch;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 
 namespace ProfileSearcher.ViewModel
 {
+    public class Field
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public Field(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+
     public class MainViewModel : ViewModelBase
     {
-        private List<AnalyzedUser> data = new List<AnalyzedUser>()
+        public List<Field> Fields { get; set; } = new List<Field>
         {
-            new AnalyzedUser
-            {
-                
-            }
+            new Field("Потребител", "user"),
+            new Field("Линк към профил", "profile_url")
         };
-        private ICommand SearchCommand { get; set; }
+        public List<AnalyzedUser> Users { get; set; } = new List<AnalyzedUser>();
+
+        private Field selectedField;
+        public Field SelectedField
+        {
+            get
+            {
+                return selectedField;
+            }
+            set
+            {
+                selectedField = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICommand SearchCommand { get; private set; }
         public MainViewModel()
         {
             SearchCommand = new RelayCommand(Search);
@@ -23,7 +53,11 @@ namespace ProfileSearcher.ViewModel
 
         private void Search()
         {
-
+            if (!String.IsNullOrEmpty(query))
+            {
+                Users = ProfileSearchService.Search(query, selectedField.Value).ToList();
+                RaisePropertyChanged("Users");
+            }
         }
 
         private string query;
