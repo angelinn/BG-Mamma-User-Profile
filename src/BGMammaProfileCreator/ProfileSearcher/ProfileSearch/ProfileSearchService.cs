b9d8b9a4b4d5b8b850345772e17613fc8c366f3c
angelinn <angelin.nedelchev@outlook.com>
2018-01-27 14:03:50 +0200

    Add profile search service.

---
+++ src/BGMammaProfileCreator/ProfileSearcher/ProfileSearch/ProfileSearchService.cs
@@ -0,0 +1,20 @@
+﻿using ProfileCreator;
+using ProfileCreator.Models.Analyzed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileSearcher.ProfileSearch
+{
+    public class ProfileSearchService
+    {
+        public static IEnumerable<AnalyzedUser> Search(string query, string field)
+        {
+            ProfileService service = new ProfileService();
+            IEnumerable<DocumentFrequency> results = service.Search(query, field);
+            return service.GetFrequencies(results);
+        }
+    }
+}
