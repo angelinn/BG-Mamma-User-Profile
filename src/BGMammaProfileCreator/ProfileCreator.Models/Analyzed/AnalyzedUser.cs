04f31c1b40836a420fa5c36353db26e9bff185ca
angelinn <angelin.nedelchev@outlook.com>
2018-01-27 14:03:33 +0200

    Add object to represent user after analyzation.

---
+++ src/BGMammaProfileCreator/ProfileCreator.Models/Analyzed/AnalyzedUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProfileCreator.Models.Analyzed
+{
+    public class AnalyzedUser
+    {
+        public string Username { get; set; }
+        public string ProfileUrl { get; set; }
+        public Dictionary<string, float> Characteristics { get; set; }
+        public List<KeyValuePair<string, float>> List
+        {
+            get
+            {
+                return Characteristics.Select(k => k).OrderByDescending(p => p.Value).ToList();
+            }
+        }
+
+        public string Classes => String.Join(", ", List.Select(p => $"{p.Key} - {p.Value}"));
+        
+    }
+}
